dfc7ac6484dd8e8194b7514ded99e9c8cf9ad7d6
Tom_Freier <Thomas@freier-online.de>
2022-08-29 20:20:31 +0200

    Display Demo added

--- ESPHubHWTest/src/main.cpp
+++ ESPHubHWTest/src/main.cpp
@@ -20,7 +20,7 @@
 #include "PCF8574.h"
 #include "Wire.h"
 #include "SSD1306Wire.h"
-//#include "images.h"
+#include "images.h"
 
 const String sSoftware = "ESPHubHWTest V0.3";
 
@@ -63,20 +63,30 @@ const int SCL_PIN = D1;
 //const int SCL_PIN = D4; //GPIO2
 
 // Initialize the oled display for address 0x3c
-SSD1306Wire display(0x3c,D2,D1);
-/*
+SSD1306Wire display(0x3c,SDA_PIN,SCL_PIN);
+
 #define DEMO_DURATION 3000
 typedef void (*Demo)(void);
-
-int demoMode = 0;
-int counter = 1;
-*/
 
 
 // Forward declarations
 void ledOn(uint8_t LedNr);
 void ledOff(uint8_t LedNr);
 void drawFontFaceDemo();
+void drawTextFlowDemo();
+void drawTextAlignmentDemo();
+void drawRectDemo();
+void drawCircleDemo();
+void drawProgressBarDemo();
+void drawImageDemo() ;
+
+Demo demos[] = {drawFontFaceDemo, drawTextFlowDemo, drawTextAlignmentDemo, drawRectDemo, drawCircleDemo, drawProgressBarDemo, drawImageDemo};
+int demoMode = 0;
+int counter = 1;
+int demoLength = (sizeof(demos) / sizeof(Demo));
+long timeSinceLastModeSwitch = 0;
+
+
 
 int ledNr = 0;
 
@@ -117,7 +127,7 @@ void setup()
   display.setFont(ArialMT_Plain_16);
  // display.setTextAlignment(TEXT_ALIGN_CENTER);
  // display.setContrast(255);
-    display.drawString(2, 10, "Hello world2");
+    display.drawString(2, 10, "ESP HUB Test");
     display.display();
     delay(1000);
   Serial.println("setup end");
@@ -129,7 +139,22 @@ void loop()
   // put your main code here, to run repeatedly:
 // clear the display
  
-  display.drawString(0, 0, "Hello world3");
+  display.clear();
+ // draw the current demo method
+  demos[demoMode]();
+
+  display.setFont(ArialMT_Plain_10);
+  display.setTextAlignment(TEXT_ALIGN_RIGHT);
+  display.drawString(128, 54, String(millis()));
+  // write the buffer to the display
+  display.display();
+
+  if (millis() - timeSinceLastModeSwitch > DEMO_DURATION) {
+    demoMode = (demoMode + 1)  % demoLength;
+    timeSinceLastModeSwitch = millis();
+  }
+  counter++;
+  delay(10);
 
   digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
   delay(500);                      // wait for a second
@@ -188,4 +213,73 @@ void drawFontFaceDemo() {
   display.drawString(0, 10, "Hello world");
   display.setFont(ArialMT_Plain_24);
   display.drawString(0, 26, "Hello world");
+}
+
+void drawTextFlowDemo() {
+  display.setFont(ArialMT_Plain_10);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.drawStringMaxWidth(0, 0, 128,
+                             "Lorem ipsum\n dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore." );
+}
+
+void drawTextAlignmentDemo() {
+  // Text alignment demo
+  display.setFont(ArialMT_Plain_10);
+
+  // The coordinates define the left starting point of the text
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.drawString(0, 10, "Left aligned (0,10)");
+
+  // The coordinates define the center of the text
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.drawString(64, 22, "Center aligned (64,22)");
+
+  // The coordinates define the right end of the text
+  display.setTextAlignment(TEXT_ALIGN_RIGHT);
+  display.drawString(128, 33, "Right aligned (128,33)");
+}
+
+void drawRectDemo() {
+  // Draw a pixel at given position
+  for (int i = 0; i < 10; i++) {
+    display.setPixel(i, i);
+    display.setPixel(10 - i, i);
+  }
+  display.drawRect(12, 12, 20, 20);
+
+  // Fill the rectangle
+  display.fillRect(14, 14, 17, 17);
+
+  // Draw a line horizontally
+  display.drawHorizontalLine(0, 40, 20);
+
+  // Draw a line horizontally
+  display.drawVerticalLine(40, 0, 20);
+}
+
+void drawCircleDemo() {
+  for (int i = 1; i < 8; i++) {
+    display.setColor(WHITE);
+    display.drawCircle(32, 32, i * 3);
+    if (i % 2 == 0) {
+      display.setColor(BLACK);
+    }
+    display.fillCircle(96, 32, 32 - i * 3);
+  }
+}
+
+void drawProgressBarDemo() {
+  int progress = (counter / 5) % 100;
+  // draw the progress bar
+  display.drawProgressBar(0, 32, 120, 10, progress);
+
+  // draw the percentage as String
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.drawString(64, 15, String(progress) + "%");
+}
+
+void drawImageDemo() {
+  // see http://blog.squix.org/2015/05/esp8266-nodemcu-how-to-create-xbm.html
+  // on how to create xbm files
+  display.drawXbm(34, 14, WiFi_Logo_width, WiFi_Logo_height, WiFi_Logo_bits);
 }
