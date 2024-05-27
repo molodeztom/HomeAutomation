8d53f65692d90f2c9510f70171176d9679bb7fe6
Tom_Freier <Thomas@freier-online.de>
2024-05-27 20:53:21 +0200

    S3 test changes

--- ESP32-S3 Test/src/main.cpp
+++ ESP32-S3 Test/src/main.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************
 ESP32 Test Project
-   
+
 
   Hardware:
   ESP32-S3-DevKitC-1 mit Wroom N16R8
@@ -16,9 +16,9 @@ ESP32 Test Project
 */
 #include <Arduino.h>
 
-#define  BUTTON_PIN_BITMASK 0xF0000000
+#define BUTTON_PIN_BITMASK 0x8000
 
-const String sSoftware = "ESP32 DeepSleep Tests V0.1";
+const String sSoftware = "ESP32 DeepSleep Tests V0.2";
 
 RTC_DATA_ATTR int bootCount = 0;
 
@@ -26,18 +26,19 @@ RTC_DATA_ATTR int bootCount = 0;
 int myFunction(int, int);
 void print_wakeup_reason();
 
-void setup() {
-    Serial.begin(56000);
-    Serial.flush();
-    delay(2000);
-    Serial.println();
-    Serial.println(sSoftware);
-    ++bootCount;
-    delay(500);
-    Serial.println("in setup routine");
-    Serial.println("Boot Nr.: " + String(bootCount));
-    print_wakeup_reason();
-    esp_sleep_enable_timer_wakeup(10e+6);
+void setup()
+{
+  Serial.begin(56000);
+  Serial.flush();
+  delay(2000);
+  Serial.println();
+  Serial.println(sSoftware);
+  ++bootCount;
+  delay(500);
+  Serial.println("in setup routine");
+  Serial.println("Boot Nr.: " + String(bootCount));
+  print_wakeup_reason();
+  esp_sleep_enable_timer_wakeup(10e+5);
   /*
     First we configure the wake up source
     We set our ESP32 to wake up for an external trigger.
@@ -48,42 +49,53 @@ void setup() {
     Note that using internal pullups/pulldowns also requires
     RTC peripherals to be turned on.
   */
-  esp_sleep_enable_ext0_wakeup(GPIO_NUM_15,1); //1 = High, 0 = Low
-    //esp_deep_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
-
-  
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_15, 1); // 1 = High, 0 = Low
+  /*   esp_sleep_enable_ext1_wakeup(BUTTON_PIN_BITMASK, ESP_EXT1_WAKEUP_ANY_HIGH);
+    gpio_pullup_dis(GPIO_NUM_15);
+    gpio_pulldown_dis(GPIO_NUM_15);
+   */
 }
 
-void loop() {
-Serial.println("Loop Start");
-neopixelWrite(RGB_BUILTIN,0,60,0); // Green
+void loop()
+{
+  Serial.println("Loop Start");
+  neopixelWrite(RGB_BUILTIN, 0, 60, 0); // Green
 
-delay(5000);
+  delay(2000);
 
-neopixelWrite(RGB_BUILTIN,0,0,0); // Red
-delay(10000);
+  neopixelWrite(RGB_BUILTIN, 0, 0, 0); // Red
+  delay(1000);
 
-Serial.println("Loop End");
-Serial.flush();
-esp_deep_sleep_start();
-
-
- 
+  Serial.println("Loop End");
+  Serial.flush();
+  esp_deep_sleep_start();
 }
 
-void print_wakeup_reason(){
+void print_wakeup_reason()
+{
   esp_sleep_wakeup_cause_t wakeup_reason;
 
   wakeup_reason = esp_sleep_get_wakeup_cause();
 
-  switch(wakeup_reason)
+  switch (wakeup_reason)
   {
-    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
-    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
-    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
-    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
-    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
-    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  case ESP_SLEEP_WAKEUP_EXT0:
+    Serial.println("Wakeup caused by external signal using RTC_IO");
+    break;
+  case ESP_SLEEP_WAKEUP_EXT1:
+    Serial.println("Wakeup caused by external signal using RTC_CNTL");
+    break;
+  case ESP_SLEEP_WAKEUP_TIMER:
+    Serial.println("Wakeup caused by timer");
+    break;
+  case ESP_SLEEP_WAKEUP_TOUCHPAD:
+    Serial.println("Wakeup caused by touchpad");
+    break;
+  case ESP_SLEEP_WAKEUP_ULP:
+    Serial.println("Wakeup caused by ULP program");
+    break;
+  default:
+    Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason);
+    break;
   }
 }
-
